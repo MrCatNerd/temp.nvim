d584a934a76df87b0c88102fdf5d4327c0c2580f
MrCatNerd <106884124+MrCatNerd@users.noreply.github.com>
2023-10-06 21:18:53 -0700

    oopsie

--- plugin/ucommands.lua
+++ plugin/ucommands.lua
@@ -1,5 +1,5 @@
 local temp = require("temp.init")
 
-vim.api.nvim_create_user_command("GutenMachine", function ()
+vim.api.nvim_create_user_command("TempToggleMenu", function ()
     temp.toggle_menu()
 end)
